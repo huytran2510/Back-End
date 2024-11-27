b64a8584e4c6e1a6a44f25ee29dbb5d6751e89e9
huytran2510 <huy251003@gmail.com>
2024-11-27 14:25:27 +0700

    update VNPayConfig

--- src/main/java/com/example/backendproject/BackEndProjectApplication.java
+++ src/main/java/com/example/backendproject/BackEndProjectApplication.java
@@ -1,6 +1,9 @@
 package com.example.backendproject;
 
+import com.example.backendproject.domain.model.Customer;
 import com.example.backendproject.domain.model.User;
+import com.example.backendproject.domain.model.enums.Gender;
+import com.example.backendproject.repository.CustomerRepository;
 import com.example.backendproject.repository.UserRepository;
 import com.example.backendproject.util.CustomPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,29 +21,29 @@ public class BackEndProjectApplication {
 
 	}
 
-//	@Autowired
-//	CustomPasswordEncoder passwordEncoder;
-//
-//	@Bean
-//	CommandLineRunner runner(UserRepository userRepository, CustomerRepository customerRepository) {
-//		return args -> {
-//			// Create a new user
-//			String username = "testUser";
-//			Gender gender = Gender.MALE;
-//			String password = "password123";
-//			String firstName = "John";
-//			String lastName = "Doe";
-//			String email = "johndoe@example.com";
-//			String phone = "123456789";
-//			String address = "123 Main St";
-//			LocalDate birthday = LocalDate.of(1990, 1, 1);
-//			Customer customer = new Customer(username, gender, passwordEncoder.getPasswordEncoder().encode("test1"), firstName, lastName, email, phone, address, birthday);
-//
-//			// Save the user to the database
-//			customerRepository.save(customer);
-//
-//			System.out.println("User inserted into the database: " + customer.getUsername());
-//		};
-//	}
+	@Autowired
+	CustomPasswordEncoder passwordEncoder;
+
+	@Bean
+	CommandLineRunner runner(UserRepository userRepository, CustomerRepository customerRepository) {
+		return args -> {
+			// Create a new user
+			String username = "testUser";
+			Gender gender = Gender.MALE;
+			String password = "password123";
+			String firstName = "John";
+			String lastName = "Doe";
+			String email = "johndoe@example.com";
+			String phone = "123456789";
+			String address = "123 Main St";
+			LocalDate birthday = LocalDate.of(1990, 1, 1);
+			Customer customer = new Customer(username, gender, passwordEncoder.getPasswordEncoder().encode("test1"), firstName, lastName, email, phone, address, birthday);
+
+			// Save the user to the database
+			customerRepository.save(customer);
+
+			System.out.println("User inserted into the database: " + customer.getUsername());
+		};
+	}
 
 }
--- src/main/java/com/example/backendproject/controller/ChatController.java
+++ src/main/java/com/example/backendproject/controller/ChatController.java
@@ -9,23 +9,43 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
+import java.security.Principal;
+import java.util.UUID;
+
 @Controller
 public class ChatController {
 
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
+//    @MessageMapping("/message")
+//    @SendTo("/chatroom/public")
+//    public Messages receiveMessage(@Payload Messages message){
+//        System.out.println(message);
+//        return message;
+//    }
+//
+//    @MessageMapping("/private-message")
+//    public Messages recMessage(@Payload Messages message){
+//        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(),"/private",message);
+//        System.out.println(message.toString());
+//        return message;
+//    }
+
     @MessageMapping("/message")
-    @SendTo("/chatroom/public")
-    public Messages receiveMessage(@Payload Messages message){
-        System.out.println(message);
+    public Messages sendMessage(@Payload Messages message, Principal principal) {
+        String senderName = principal == null ? "client-" + UUID.randomUUID() : principal.getName();
+        message.setSenderName(senderName);
+
+        // Tin nhắn sẽ gửi đến "staff"
+        simpMessagingTemplate.convertAndSendToUser("staff", "/private", message);
         return message;
     }
 
-    @MessageMapping("/private-message")
-    public Messages recMessage(@Payload Messages message){
-        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(),"/private",message);
-        System.out.println(message.toString());
+    @MessageMapping("/reply")
+    public Messages replyMessage(@Payload Messages message) {
+        // Nhân viên trả lời khách hàng (theo tên khách hàng gửi đến)
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/private", message);
         return message;
     }
 }
